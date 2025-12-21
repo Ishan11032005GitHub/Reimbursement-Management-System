fc403b24c1b48dd686e4d23b0497d1f0463f9bc4
Ishan11032005GitHub <Ishan11032005@gmail.com>
2025-12-21 20:02:14 +0530

    Code Pushed

--- rms-frontend/src/api/axios.js
+++ rms-frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://reimbursement-management-system.onrender.com/api"
+  baseURL: "https://reimbursement-management-system.onrender.com/api",
+  withCredentials: true
 });
 
 api.interceptors.request.use(config => {
