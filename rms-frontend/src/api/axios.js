d6a90c6d496a9b73da1f387da9e2676ff25da6af
Ishan11032005GitHub <Ishan11032005@gmail.com>
2026-01-02 15:03:02 +0530

    fix: add recharts dependency

--- rms-frontend/src/api/axios.js
+++ rms-frontend/src/api/axios.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://reimbursement-management-system.onrender.com/api",
 });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
