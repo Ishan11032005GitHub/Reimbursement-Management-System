9ef0a9654269829e321eb942962c86ccd20a8844
Ishan11032005GitHub <Ishan11032005@gmail.com>
2026-01-05 00:50:15 +0530

    ...

--- rms-backend/utils/email.js
+++ rms-backend/utils/email.js
@@ -1,20 +1,18 @@
-// utils/email.js
 const { Resend } = require("resend");
-
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendResetEmail(to, resetUrl) {
-  await resend.emails.send({
+  const result = await resend.emails.send({
     from: process.env.EMAIL_FROM,
     to,
     subject: "Reset your password",
     html: `
-      <h2>Password Reset</h2>
-      <p>Click the link below to reset your password.</p>
-      <a href="${resetUrl}">Reset Password</a>
-      <p>This link expires in 15 minutes.</p>
+      <p>Reset your password:</p>
+      <a href="${resetUrl}">${resetUrl}</a>
     `
   });
+
+  console.log("RESEND RESULT:", result);
 }
 
 module.exports = { sendResetEmail };
